c57553e5cf1090a9c26529037c7b1e01e0d906f2
Jb-Projects-code <julkarnainbhuiyan1@gmail.com>
2026-03-13 16:36:19 +0000

    Add files via upload

---
+++ Password_Strength_Checker.py
@@ -0,0 +1,38 @@
+import string
+
+password = input("Password: ")
+
+score = 0 
+
+#check for the length
+if len(password) >= 8: 
+    score += 1
+
+#check for lowercase
+if any(char.islower() for char in password):
+    score += 1
+
+#check for upper#
+if any(char.isupper() for char in password):
+    score += 1
+
+#check for digits
+if any(char.isdigit() for char in password):
+    score +=1
+
+#check for special digits
+if any(char is string.punctuation for char in password):
+    score += 1 
+
+#strength rating
+if score >= 5:
+    print("password strength: strong")
+
+elif score >= 3:
+    print("password strength: medium")
+
+elif score >3: 
+    print("Password strength: weak") 
+
+elif score >= 2:
+    print("There is something wrong with your brain u bastard")
